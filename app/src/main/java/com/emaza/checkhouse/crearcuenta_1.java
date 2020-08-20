062cde03832771c9b65bbe9e717f40d83de8df19
Eric Maza <avantica-eric.maza@mibanco.com.pe>
2020-08-19 23:29:50 -0500

    initial

---
+++ app/src/main/java/com/emaza/checkhouse/crearcuenta_1.java
@@ -0,0 +1,14 @@
+package com.emaza.checkhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class crearcuenta_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crearcuenta_1);
+    }
+}
---
+++ app/src/main/java/com/emaza/checkhouse/sucessful_access.java
@@ -0,0 +1,14 @@
+package com.emaza.checkhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sucessful_access extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucessful_access);
+    }
+}
