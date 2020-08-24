741ddb620dda39ef8cef22919e3b0ba7394b1bc8
Eric Maza <avantica-eric.maza@mibanco.com.pe>
2020-08-23 20:44:22 -0500

    compararDirecciones

---
+++ app/src/main/java/com/emaza/checkhouse/Act_CompararDirecciones.java
@@ -0,0 +1,14 @@
+package com.emaza.checkhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_CompararDirecciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_comparar_direcciones);
+    }
+}
