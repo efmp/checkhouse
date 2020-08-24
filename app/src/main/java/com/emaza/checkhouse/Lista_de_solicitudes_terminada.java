9710295658ad2fe475fa1ce23a5ff6b6740df1d0
Roy Miguel Prado Torres <52686131+roympradotorres@users.noreply.github.com>
2020-08-23 20:28:42 -0500

    prototipo-roy

---
+++ app/src/main/java/com/emaza/checkhouse/Lista_de_solicitudes_terminada.java
@@ -0,0 +1,14 @@
+package com.emaza.checkhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista_de_solicitudes_terminada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_solicitudes_terminada);
+    }
+}
---
+++ app/src/main/java/com/emaza/checkhouse/lista_de_solicitudes_vacia.java
@@ -0,0 +1,14 @@
+package com.emaza.checkhouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lista_de_solicitudes_vacia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_solicitudes_vacia);
+    }
+}
