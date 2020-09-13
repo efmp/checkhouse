255b20756617411074c3e07a32e1a7dd007528ca
Eric Maza <avantica-eric.maza@mibanco.com.pe>
2020-09-13 18:00:55 -0500

    mantenimiento

--- app/src/main/java/com/emaza/checkhouse/crearcuenta_1.java
+++ app/src/main/java/com/emaza/checkhouse/crearcuenta_1.java
@@ -3,12 +3,25 @@ package com.emaza.checkhouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class crearcuenta_1 extends AppCompatActivity {
 
+    TextView txtcorreo, txtcelular;
+    Button btnContinuar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crearcuenta_1);
+        asignarReferencias();
     }
+
+    private void asignarReferencias() {
+        txtcorreo = findViewById(R.id.txtCorreo);
+        txtcelular = findViewById(R.id.txtTelefono);
+        btnContinuar = findViewById(R.id.btnContinuar);
+    }
+
+
 }
---
+++ app/src/main/java/com/emaza/checkhouse/entidades/Usuario.java
@@ -0,0 +1,110 @@
+package com.emaza.checkhouse.entidades;
+
+public class Usuario {
+
+    public Usuario(int id, String nombres, String apellidos, String correo, String dni, int idvivienda, String password, int intentos, String tipo, String estado) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.dni = dni;
+        this.idvivienda = idvivienda;
+        this.password = password;
+        this.intentos = intentos;
+        this.tipo = tipo;
+        this.estado = estado;
+    }
+
+
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String dni;
+    private int idvivienda;
+    private String password;
+    private int intentos;
+    private String tipo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getIdvivienda() {
+        return idvivienda;
+    }
+
+    public void setIdvivienda(int idvivienda) {
+        this.idvivienda = idvivienda;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIntentos() {
+        return intentos;
+    }
+
+    public void setIntentos(int intentos) {
+        this.intentos = intentos;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    private String estado;
+}
