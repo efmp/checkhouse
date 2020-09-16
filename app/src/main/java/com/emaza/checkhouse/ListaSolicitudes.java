3bea01c0ca871e7b786644d1b8f4adc680b8a89b
Eric Maza <avantica-eric.maza@mibanco.com.pe>
2020-09-16 01:03:05 -0500

    listview personalizado terminado

--- app/src/main/java/com/emaza/checkhouse/ListaSolicitudes.java
+++ app/src/main/java/com/emaza/checkhouse/ListaSolicitudes.java
@@ -17,6 +17,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.emaza.checkhouse.adaptadores.solicitudesAdapter;
 import com.emaza.checkhouse.entidades.Solicitud;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -32,10 +33,17 @@ import java.util.concurrent.CountDownLatch;
 
 public class ListaSolicitudes extends AppCompatActivity {
 
+    //String[][] datos;
+//            = {
+//            {"1","4","Juan","apellidos","32165487",null,"Interbank","Pendiente"},
+//            {"2","4","Juan","Peru","12345678",null,"BCP","Pendiente"}
+//    };
+    int[] datosImg = {R.drawable.bcpicon,R.drawable.interbank};
+
 
     String[] from = new String[]{"descripcion","estado"};
     int[] to = new int[]{R.id.txtDescripcion, R.id.txtEstado};
-    private ArrayList<Solicitud> listaSolicitudes;
+
     JsonObject usuario_json;
     TextView lblNombreUsuario;
     ListView lstSolicitudes;
@@ -53,25 +61,23 @@ public class ListaSolicitudes extends AppCompatActivity {
             usuario_json = new JsonParser().parse(data).getAsJsonObject();
         }
         asignarReferencias();
-        accionGetDataSolicitud();
-        //obtenerData();
-        //mostrarSolicitudes();
+        //accionGetDataSolicitud();
     }
 
     private void asignarReferencias() {
         lstSolicitudes = findViewById(R.id.lstSolicitudes);
         lblNombreUsuario = findViewById(R.id.lblNombre);
         lblNombreUsuario.setText(usuario_json.get("nombres").getAsString());
-        listaSolicitudes = new ArrayList<>();
         lblNombreUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //obtenerData();
                 //mostrarSolicitudes();
                 accionGetDataSolicitud();
-                System.out.println("DATA:"+scode);
+
             }
         });
+
 
         lstSolicitudes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -79,6 +85,8 @@ public class ListaSolicitudes extends AppCompatActivity {
 
             }
         });
+
+        //lstSolicitudes.setAdapter(new solicitudesAdapter(this,datos,datosImg));
     }
 
     String scode;
@@ -91,9 +99,9 @@ public class ListaSolicitudes extends AppCompatActivity {
                 if (codigo == null) {//si el código recibido es null
                     Toast.makeText(ListaSolicitudes.this,"Sin respuesta del servidor\nIntentelo de nuevo",Toast.LENGTH_SHORT).show();
                 } else {
-                    System.out.println(codigo);
-                    transformarData(codigo);
-                    mostrarSolicitudes();
+                    System.out.println("Resultado"+codigo);
+                    String[][] datos = transformarData(codigo);
+                    lstSolicitudes.setAdapter(new solicitudesAdapter(ListaSolicitudes.this,datos,datosImg));
                     Toast.makeText(ListaSolicitudes.this,"Estado 200 OK",Toast.LENGTH_SHORT).show();
 
                     if (codigo.contains("<0>")){
@@ -136,46 +144,23 @@ public class ListaSolicitudes extends AppCompatActivity {
         void onResponseData(String data);
     }
 
-    public void transformarData(String data){
+    public String[][] transformarData(String data){
         JsonArray jsonArray = new Gson().fromJson(data,JsonArray.class);
+        String[][] datos = new String[jsonArray.size()][8];
         for (int i=0;i<jsonArray.size();i++){
-            Solicitud solicitud = new Solicitud(
-                    jsonArray.get(i).getAsJsonObject().get("id").getAsInt(),
-                    jsonArray.get(i).getAsJsonObject().get("usuario").getAsInt(),
-                    jsonArray.get(i).getAsJsonObject().get("nombres").getAsString(),
-                    jsonArray.get(i).getAsJsonObject().get("apellidos").getAsString(),
-                    jsonArray.get(i).getAsJsonObject().get("dni").getAsString(),
-                    jsonArray.get(i).getAsJsonObject().get("banco").getAsString(),
-                    jsonArray.get(i).getAsJsonObject().get("estado").getAsString());
-            listaSolicitudes.add(solicitud);
+            datos[i][0] = jsonArray.get(i).getAsJsonObject().get("id").getAsString();
+            datos[i][1] = jsonArray.get(i).getAsJsonObject().get("usuario").getAsString();
+            datos[i][2] = jsonArray.get(i).getAsJsonObject().get("nombres").getAsString();
+            datos[i][3] = jsonArray.get(i).getAsJsonObject().get("apellidos").getAsString();
+            datos[i][4] = jsonArray.get(i).getAsJsonObject().get("dni").getAsString();
+            datos[i][5] = null;
+            datos[i][6] = jsonArray.get(i).getAsJsonObject().get("banco").getAsString();
+            datos[i][7] = jsonArray.get(i).getAsJsonObject().get("estado").getAsString();
         }
-        System.out.println(listaSolicitudes.get(0).getNombres());
-        System.out.println("orueta");
+        System.out.println("dataobjeto:"+datos[0][0]);
+        return  datos;
     }
 
-    public void mostrarSolicitudes(){
-        //listaProductos = daoProducto.cargarProductos();
-        if (listaSolicitudes.size()<1){
 
-        }else{
-            ArrayList<HashMap<String,String>> productos = new ArrayList<>();
-            for(Solicitud solicitud:listaSolicitudes){
-                HashMap<String,String> dataProducto = new HashMap<>();
-                dataProducto.put("id", String.valueOf(solicitud.getId()));
-                dataProducto.put("usuario", String.valueOf(solicitud.getIdusuario()));
-                dataProducto.put("nombres", "S/. "+solicitud.getNombres());
-                dataProducto.put("apellidos", solicitud.getApellidos());
-                dataProducto.put("dni", solicitud.getDni());
-                dataProducto.put("banco", solicitud.getBanco());
-                dataProducto.put("estado",solicitud.getEstado());
-                dataProducto.put("descripcion",solicitud.getBanco()+" ha solicitado que Ud. realice la verificación de su domicilio");
-                dataProducto.put("btnempezar","Empezar2");
-                productos.add(dataProducto);
-                lstSolicitudes = findViewById(R.id.lstSolicitudes);
-                SimpleAdapter adapter = new SimpleAdapter(this, productos, R.layout.solicitud_modelo, from, to);
-                lstSolicitudes.setAdapter(adapter);
-            }
-        }
-    }
 
 }
---
+++ app/src/main/java/com/emaza/checkhouse/adaptadores/solicitudesAdapter.java
@@ -0,0 +1,98 @@
+package com.emaza.checkhouse.adaptadores;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.emaza.checkhouse.Maps;
+import com.emaza.checkhouse.R;
+
+public class solicitudesAdapter extends BaseAdapter {
+
+//    {"1","4","Juan","apellidos","32165487",null,"Interbank","Pendiente"},
+//    {"2","4","Juan","Peru","12345678",null,"BCP","Pendiente"}
+    //" ha solicitado que Ud. realice la verificación de su domicilio")
+    private static LayoutInflater  inflater = null;
+    Context contexto;
+    String[][] datos;
+    int[] datosImg;
+
+    TextView txtDescripcion, txtEstado;
+    ImageView imgBanco;
+    Button btnEmpezar;
+
+    public solicitudesAdapter(Context contexto, String[][] datos, int[] datosImg) {
+        this.contexto = contexto;
+        this.datos = datos;
+        this.datosImg = datosImg;
+        inflater = (LayoutInflater)contexto.getSystemService(contexto.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final View vista = inflater.inflate(R.layout.solicitud_modelo, null);
+
+        txtDescripcion = vista.findViewById(R.id.txtDescripcion);
+        txtEstado = vista.findViewById(R.id.txtEstado);
+        imgBanco = vista.findViewById(R.id.imgBanco);
+        btnEmpezar = vista.findViewById(R.id.btnEmpezar);
+
+
+        String texto= datos[i][6]+" ha solicitado que Ud. realice la verificación de su domicilio";
+        txtDescripcion.setText(texto);
+        txtEstado.setText(datos[i][7]);
+        int indexImagen=0;
+        if(datos[i][6].equals("Interbank")){
+            indexImagen=1;
+        }else if(datos[i][6].equals("BCP")){
+            indexImagen=0;
+        }
+        imgBanco.setImageResource(datosImg[indexImagen]);
+        imgBanco.setTag(indexImagen);
+        btnEmpezar.setTag(i);
+        btnEmpezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Hacer Intent
+                Intent visorImagen = new Intent(contexto, Maps.class);
+                //enviar Data
+                contexto.startActivity(visorImagen);
+            }
+        });
+
+
+        return vista;
+    }
+
+//        dataProducto.put("id", String.valueOf(solicitud.getId()));
+//        dataProducto.put("usuario", String.valueOf(solicitud.getIdusuario()));
+//        dataProducto.put("nombres", "S/. "+solicitud.getNombres());
+//        dataProducto.put("apellidos", solicitud.getApellidos());
+//        dataProducto.put("dni", solicitud.getDni());
+//        dataProducto.put("banco", solicitud.getBanco());
+//        dataProducto.put("estado",solicitud.getEstado());
+//        dataProducto.put("descripcion",solicitud.getBanco()+" ha solicitado que Ud. realice la verificación de su domicilio");
+//        dataProducto.put("btnempezar","Empezar2");
+    @Override
+    public int getCount() {
+        return datosImg.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+
+}
