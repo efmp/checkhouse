81109da49f9763a2f2e46b11e0c9df9669f4d488
D-a-x <dax_bm@hotmail.com>
2020-09-20 19:13:26 -0500

    Subida la parte abrir la galeria

--- app/src/main/java/com/emaza/checkhouse/agregar_foto.java
+++ app/src/main/java/com/emaza/checkhouse/agregar_foto.java
@@ -2,9 +2,13 @@ package com.emaza.checkhouse;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -12,40 +16,61 @@ import android.widget.ImageView;
 
 public class agregar_foto extends AppCompatActivity {
 
-    ImageView imagen;
+    private ImageView mimageView;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_foto);
+        mimageView = findViewById(R.id.imgfoto);
 
 
-        imagen=findViewById(R.id.imagenid);
 
+        if (ContextCompat.checkSelfPermission(agregar_foto.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(agregar_foto.this, new String[]
+                    {
+                            Manifest.permission.CAMERA
+                    }, 100);
+        }
 
     }
 
 
-    public void onclic(View view) {
-        cargarimagen();
+
+
+    public void TomaFoto(View view)
+    {
+        Intent imageTakenIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageTakenIntent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivityForResult(imageTakenIntent,REQUEST_IMAGE_CAPTURE);
+        }
     }
 
-    private void cargarimagen() {
-        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/");
-        startActivityForResult(intent.createChooser(intent,"Seleccione la Aplicación"),10);
+
+    public void TomaFoto2(View view)
+    {
+        Intent imageTakenIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageTakenIntent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivityForResult(imageTakenIntent,REQUEST_IMAGE_CAPTURE);
+        }
     }
+
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK){
-            Uri path=data.getData();
-            imagen.setImageURI(path);
-
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mimageView.setImageBitmap(imageBitmap);
         }
-
-
-    }
-
-}
+    }}
