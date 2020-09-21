871ab9d2c7a2d1da3972b44a7aee080a581618ae
Eric Maza <avantica-eric.maza@mibanco.com.pe>
2020-09-21 01:49:01 -0500

    avance con registro vivienda

--- app/src/main/java/com/emaza/checkhouse/MapsActivity.java
+++ app/src/main/java/com/emaza/checkhouse/MapsActivity.java
@@ -9,6 +9,13 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,9 +28,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -31,6 +42,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private DatabaseReference mDatabase;
     private ArrayList<Marker> tmpRealTimeMarkers = new ArrayList<>();
     private ArrayList<Marker> realTimeMarkers = new ArrayList<>();
+
+    double latitud;
+    double longitud;
+    String local_idvivienda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +68,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onFinish(){
                 Toast.makeText(MapsActivity.this,"Se verifico los puntos de su ubicación y seran evaluadas", Toast.LENGTH_SHORT).show();
                 onMapReady(mMap);
+
+                final String url = "http://checkhouseapi.atwebpages.com/index.php/nuevavivienda";
+                JsonObject objeto = new JsonObject();
+                objeto.addProperty("latitud", String.valueOf(latitud));
+                objeto.addProperty("longitud", String.valueOf(longitud));
+                hacerPostDetalleSolicitud(url, new DataResponseListener() {
+                    @Override
+                    public void onResponseData(String data) {
+                        if(!data.equals("")){
+                            System.out.println("RESPUESTA:"+data);
+                            JsonObject json = new JsonParser().parse(data).getAsJsonObject();
+                            String idvivienda = json.get("data").getAsJsonObject().get("id").getAsString();
+                            local_idvivienda = idvivienda;
+                            System.out.println("SE OBTUVO VIVIENDA: "+idvivienda);
+
+                        }
+                        else{
+                            System.err.println("Error en el servicio de detalle");
+                        }
+
+                    }
+                },objeto);
+
+
             }
         }.start();
     }
@@ -80,8 +119,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
                     MapsPojo mp = snapshot.getValue(MapsPojo.class);
-                    Double latitud = mp.getLatitud();
-                    Double longitud = mp.getLongitud();
+                    latitud = mp.getLatitud();
+                    longitud = mp.getLongitud();
                     MarkerOptions markerOptions = new MarkerOptions();
                     markerOptions.position(new LatLng(latitud,longitud));
                     tmpRealTimeMarkers.add(mMap.addMarker(markerOptions));
@@ -101,6 +140,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     }
+
+
+    String scode;
+    private void hacerPostDetalleSolicitud(String url, final DataResponseListener mListener, final JsonObject vivienda){
+        scode=null;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response == "") scode = "Sin respuesta";
+                        else scode = response;
+                        if (mListener != null){
+                            mListener.onResponseData(scode);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                scode = error.getMessage();
+                if (mListener != null){
+                    mListener.onResponseData(scode);
+                }
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("direccion", "null");
+                parametros.put("latitud", vivienda.get("latitud").getAsString());
+                parametros.put("longitud", vivienda.get("longitud").getAsString());
+                return parametros;
+            }
+        };
+        queue.add(stringRequest);
+    }
+//endregion
+
+    public interface DataResponseListener {
+        void onResponseData(String data);
+    }
+
 
 
 }
